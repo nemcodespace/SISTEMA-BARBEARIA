9c9de38d258e88a0f55bf4a8091ac45a687734ef
Nem code <nemcodespace@gmail.com>
2026-07-14 21:07:05 -0300

    atualizacao

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
+//menu mobile abrir e fechar
 const botao = document.getElementById("botao");
 const menu = document.getElementById("menu");
 botao.addEventListener("click", () => {
   menu.classList.toggle("ativo");
 });
+
--- dashboard.php
+++ dashboard.php
@@ -1,13 +1,40 @@
 <?php include_once("includes/header.php")?>
     <main class="main">
-      <div class="main-padding">
-        <div class="nav-top">
-          <h2><img src="assets/icons/dash2.svg" alt="dashboard">dashboard</h2>
+      <section class="main-padding">
+        <article class="nav-top">
+          <h2><img src="assets/icons/dash2.svg" alt="dashboard">Dashboard</h2>
           <div id="botao">
             <img src="assets/icons/menu.svg" alt="menu">
           </div>
-        </div>
-      </div>
+        </article>
+        <!--INICIO CARDS-->
+        <article class="cards">
+          <div class="card-azul">
+            <h3>4</h3>
+            <h4>Clientes</h4>
+          </div>
+          <div class="card-verde">
+            <h3>2</h3>
+            <h4>Usúarios</h4>
+          </div>
+          <div class="card-azul-claro">
+            <h3>6</h3>
+            <h4>Serviços</h4>
+          </div>
+          <div class="card-amarelo">
+            <h3>8</h3>
+            <h4>Agendamentos dia</h4>
+          </div>
+          <div class="card-vermelho">
+            <h3>10</h3>
+            <h4>Próximos agen...</h4>
+          </div>
+          <div>
+            6
+          </div>
+        </article>
+        <!--FIM CARDS-->
+      </section>
       <!-- footer -->
       <?php include_once("includes/footer.php")?>
     </main>
